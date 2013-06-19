913cd4673defe572e2bdb27dcef2a1a2d94e6836
mykingla <kingla_pei@163.com>
2013-06-19 15:16:12 +0800

    Add sentence validation

--- app/models/sentence.rb
+++ app/models/sentence.rb
@@ -1,6 +1,8 @@
 class Sentence < ActiveRecord::Base
   has_many :translations, :class_name => 'SentenceTranslation', :foreign_key => :sentence_id
 
+  validates :content, :presence => true
+
   def translate?(content)
     translations.map(&:content).include? content
   end
