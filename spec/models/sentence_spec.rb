188f813b3742c0eef2ab393f0aed009cf5f009c6
mykingla <kingla_pei@163.com>
2013-06-22 13:28:56 +0800

    sentence squeeze rspec

--- spec/models/sentence_spec.rb
+++ spec/models/sentence_spec.rb
@@ -14,22 +14,42 @@ describe Sentence do
      :subject => 'I am fine'
     )
 
-    @subject_1 = "I'm file"
-    @subject_2 = 'I am fine'
-    @subject_3 = 'am fine'
+    
   }
 
-  it "sentence translation(1) should be correct" do
-    @sentence.translate?(@subject_1).should == true
+  describe "Without additional spaces among words" do
+    before {
+      @subject_1 = "I'm file"
+      @subject_2 = 'I am fine'
+      @subject_3 = 'am fine'
+    }
+
+    it "sentence translation(1) should be correct" do
+      @sentence.translate?(@subject_1).should == true
+    end
+
+    it "sentence translation(2) should be correct" do
+      @sentence.translate?(@subject_2).should == true
+    end
+
+    it "sentence translation(3) should be incorrect" do
+      @sentence.translate?(@subject_3).should == false
+    end
   end
 
-  it "sentence translation(2) should be correct" do
-    @sentence.translate?(@subject_2).should == true
-  end
+  describe "With additional spaces among words" do
+    before {
+      @subject_1 = "I'm     file  "
+      @subject_2 = '   I    am    fine'
+    }
 
-  it "sentence translation(3) should be incorrect" do
-    @sentence.translate?(@subject_3).should == false
-  end
+    it "sentence translation(1) should be correct" do
+      @sentence.translate?(@subject_1).should == true
+    end
 
+    it "sentence translation(2) should be correct" do
+      @sentence.translate?(@subject_2).should == true
+    end
+  end
   
 end
