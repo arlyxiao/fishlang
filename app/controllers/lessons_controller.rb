e9f1b8fe23da2a235d582a00605c8a5ba0e653c3
mykingla <kingla_pei@163.com>
2013-06-23 12:55:58 +0800

    Init lessons controller

---
+++ app/controllers/lessons_controller.rb
@@ -0,0 +1,18 @@
+class LessonsController < ApplicationController
+
+  before_filter :pre_load
+  
+  def pre_load
+    @lesson = Lesson.find(params[:id]) if params[:id]
+  end
+
+
+  def index
+    lessons = Lesson.all
+  end
+
+  def show
+    
+  end
+
+end
