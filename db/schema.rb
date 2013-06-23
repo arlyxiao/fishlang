eb70ad4e8e11c22469420c9c5148324f01970a43
mykingla <kingla_pei@163.com>
2013-06-23 12:46:42 +0800

    Add lessons migration

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,13 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130621030635) do
+ActiveRecord::Schema.define(:version => 20130623044341) do
+
+  create_table "lessons", :force => true do |t|
+    t.string   "name"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
 
   create_table "sentence_translations", :force => true do |t|
     t.string   "subject"
