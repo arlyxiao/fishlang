e7aeae844dc49d73d6e5bf1deba1b82910c7c26c
mykingla <kingla_pei@163.com>
2013-06-24 09:42:08 +0800

    Init practice controller

---
+++ app/controllers/practices_controller.rb
@@ -0,0 +1,18 @@
+class PracticesController < ApplicationController
+
+  before_filter :pre_load
+  
+  def pre_load
+    @practice = Practice.find(params[:id]) if params[:id]
+  end
+
+
+  def index
+    @practices = Practice.all
+  end
+
+  def show
+    @sentence = @practice.sentences.first
+  end
+
+end
