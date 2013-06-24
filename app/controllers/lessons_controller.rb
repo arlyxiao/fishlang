dc388d791aceebd4ce24519bbd1240bffbe38b8d
mykingla <kingla_pei@163.com>
2013-06-24 12:27:21 +0800

    Change bootstrap theme

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -3,13 +3,10 @@ class LessonsController < ApplicationController
   before_filter :pre_load
   
   def pre_load
+    @lessons = Lesson.all
     @lesson = Lesson.find(params[:id]) if params[:id]
   end
 
-
-  def index
-    @lessons = Lesson.all
-  end
 
   def show
     @practices = @lesson.practices
