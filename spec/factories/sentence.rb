42f0efd35d5d9bb18aba4c3c3d83d0ab4bc681d4
mykingla <kingla_pei@163.com>
2013-06-27 11:32:12 +0800

    Add verb field in sentence factory data

--- spec/factories/sentence.rb
+++ spec/factories/sentence.rb
@@ -1,5 +1,6 @@
 FactoryGirl.define do
   factory :sentence do
-    sequence(:subject){|n| "content_#{n}" }    
+    sequence(:subject){|n| "content_#{n}" }
+    sequence(:verb){|n| "verb_#{n}" }
   end
 end
