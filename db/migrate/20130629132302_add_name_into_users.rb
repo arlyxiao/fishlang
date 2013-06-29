d6f0bb7541fe57583ef25ac25a35cbbddb356f66
mykingla <kingla_pei@163.com>
2013-06-29 21:32:49 +0800

    Generate devise views

---
+++ db/migrate/20130629132302_add_name_into_users.rb
@@ -0,0 +1,5 @@
+class AddNameIntoUsers < ActiveRecord::Migration
+  def change
+    add_column :users, :name, :string
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130629124815) do
+ActiveRecord::Schema.define(:version => 20130629132302) do
 
   create_table "categories", :force => true do |t|
     t.string   "name"
@@ -61,6 +61,7 @@ ActiveRecord::Schema.define(:version => 20130629124815) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                             :null => false
     t.datetime "updated_at",                             :null => false
+    t.string   "name"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
