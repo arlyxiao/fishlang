d5594e44294e0f44f0b74da5d3a926f4a6fc1003
mykingla <kingla_pei@163.com>
2013-07-05 09:39:36 +0800

    Add practices_controller rspec

---
+++ spec/controllers/practices_controller_spec.rb
@@ -0,0 +1,28 @@
+require 'spec_helper'
+
+describe PracticesController do
+  before {
+    @user = FactoryGirl.create :user
+    @practice = FactoryGirl.create :practice
+    12.times { FactoryGirl.create(:sentence, :practice => @practice) }
+  }
+
+  it "user has no any practice" do
+    @user.get_practice(@practice).should == nil
+  end
+
+  describe "#show" do
+    before {
+      sign_in @user
+      get 'show', :id => @practice.id
+    }
+
+    it "practice_id in session should be correct" do
+      session[:practice_id].should == @practice.id
+    end
+
+    it "user has practice" do
+      @user.get_practice(@practice).valid?.should == true
+    end
+  end
+end
