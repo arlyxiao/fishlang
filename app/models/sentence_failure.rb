12d9ce58457de71bc74685355558d4f2d83d601d
mykingla <kingla_pei@163.com>
2013-07-14 18:55:45 +0800

    Fix exam list on sentence_failure

--- app/models/sentence_failure.rb
+++ app/models/sentence_failure.rb
@@ -11,7 +11,7 @@ class SentenceFailure < ActiveRecord::Base
   scope :by_count, :conditions => ['correct_count < count * 3']
 
   def generate_exam
-    self.class.all.sample(10).map(&:sentence_id).to_json
+    self.class.by_count.sample(10).map(&:sentence_id).to_json
   end
 
 
