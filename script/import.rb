f407b22c2084e579547537e3fb4539adb6f33b9b
mykingla <kingla_pei@163.com>
2013-07-19 14:05:35 +0800

    Fix import

--- script/import.rb
+++ script/import.rb
@@ -31,8 +31,8 @@ def import
 
 end
 
-
-ActiveRecord::Base.connection.execute("TRUNCATE TABLE users")
+ActiveRecord::Base.connection.execute("TRUNCATE TABLE categories")
+ActiveRecord::Base.connection.execute("TRUNCATE TABLE lessons")
 ActiveRecord::Base.connection.execute("TRUNCATE TABLE practices")
 ActiveRecord::Base.connection.execute("TRUNCATE TABLE sentences")
 ActiveRecord::Base.connection.execute("TRUNCATE TABLE sentence_translations")
@@ -42,6 +42,37 @@ ActiveRecord::Base.connection.execute("TRUNCATE TABLE sentence_failures")
 ActiveRecord::Base.connection.execute("TRUNCATE TABLE lesson_points")
 ActiveRecord::Base.connection.execute("TRUNCATE TABLE practice_points")
 ActiveRecord::Base.connection.execute("TRUNCATE TABLE sentence_failure_points")
+
+
+
+c1 = Category.create(:name => 'Indicative')
+c2 = Category.create(:name => 'Subjunctive')
+c3 = Category.create(:name => 'Imperative')
+
+
+# Lessons
+l1 = Lesson.create(:category => c1, :name => 'Present')
+l2 = Lesson.create(:category => c1, :name => 'Present Perfect')
+l3 = Lesson.create(:category => c1, :name => 'Imperfect')
+l4 = Lesson.create(:category => c1, :name => 'Preterite')
+l5 = Lesson.create(:category => c1, :name => 'Past Perfect - Pluperfect')
+l6 = Lesson.create(:category => c1, :name => 'Future')
+l7 = Lesson.create(:category => c1, :name => 'Future Perfect')
+l8 = Lesson.create(:category => c1, :name => 'Condicional')
+l9 = Lesson.create(:category => c1, :name => 'Conditional Perfect')
+l10 = Lesson.create(:category => c1, :name => 'Preterite Perfect')
+
+
+l2_1 = Lesson.create(:category => c2, :name => 'Present')
+l2_2 = Lesson.create(:category => c2, :name => 'Present Perfect')
+l2_3 = Lesson.create(:category => c2, :name => 'Imperfect')
+l2_4 = Lesson.create(:category => c2, :name => 'Past Perfect - Pluperfect')
+l2_5 = Lesson.create(:category => c2, :name => 'Future')
+l2_6 = Lesson.create(:category => c2, :name => 'Future Perfect')
+
+
+l3_1 = Lesson.create(:category => c3, :name => 'Affirmative Imperative')
+l3_2 = Lesson.create(:category => c3, :name => 'Negative Commands')
 
 
 import
