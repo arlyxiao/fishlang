34fea3fd286f29433c913b80ca683349234a13f5
mykingla <kingla_pei@163.com>
2013-07-19 13:37:38 +0800

    export and import

---
+++ script/export.rb
@@ -0,0 +1,47 @@
+$dir = ARGV[0]
+
+
+def export
+  Category.all.each do |c|
+
+    path = File.join($dir, c.name)
+    FileUtils.rm_rf(path)
+    FileUtils.mkdir path
+
+    c.lessons.each do |l|
+      path = File.join($dir, c.name, l.name)
+      FileUtils.rm_rf(path)
+      FileUtils.mkdir path
+
+      l.practices.each do |p|
+
+        sentences_hash = []
+        p.sentences.each do |s|
+          row = {
+            :subject => s.subject,
+            :verb => s.verb,
+            :translations => s.translations.map(&:subject)
+          }
+          sentences_hash << row
+        end
+
+        practice_hash = {
+          :id => p.id,
+          :name => p.name,
+          :sentences => sentences_hash
+        }
+
+        target_file = File.join(path, "#{p.id}.yaml")
+        File.open(target_file, 'w+') {|f| f.write(practice_hash.to_yaml) }
+
+      end
+
+    end
+
+  end
+
+end
+
+export
+
+
---
+++ script/import.rb
@@ -0,0 +1,64 @@
+ActiveRecord::Base.connection.execute("TRUNCATE TABLE users")
+
+
+$dir = ARGV[0]
+
+
+def save_users
+	path = File.join($dir, 'users.yaml')
+	rows = YAML.load_file(path)
+	rows.each do |r|
+		user = User.create!(
+			{
+				email: r['email'], 
+				name: r['name'], 
+				password: r['password'], 
+				password_confirmation: r['password_confirmation'] 
+			}
+		)
+		user.update_attribute :admin, true
+	end
+end
+
+
+def import
+  Category.all.each do |c|
+
+    c.lessons.each do |l|
+
+      lesson_path = File.join($dir, c.name, l.name, '*.yaml')
+      practice_dirs = Dir[lesson_path]
+
+
+      practice_dirs.each do |practice_path|
+        row = YAML.load_file(practice_path)
+        practice = Practice.create(:lesson => l, :name => row[:name])
+
+        row[:sentences].each do |s|
+          sentence = Sentence.create(:practice => practice, :subject => s[:subject], :verb => s[:verb])
+          p sentence
+          s[:translations].each do |t|
+            SentenceTranslation.create(:sentence => sentence, :subject => t)
+          end
+
+        end
+
+      end
+
+    end
+
+  end
+
+end
+
+
+save_users
+
+ActiveRecord::Base.connection.execute("TRUNCATE TABLE practices")
+ActiveRecord::Base.connection.execute("TRUNCATE TABLE sentences")
+ActiveRecord::Base.connection.execute("TRUNCATE TABLE sentence_translations")
+ActiveRecord::Base.connection.execute("TRUNCATE TABLE sentence_reports")
+ActiveRecord::Base.connection.execute("TRUNCATE TABLE user_exercises")
+
+import
+
